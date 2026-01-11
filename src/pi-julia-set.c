33a51ecfb4d927697558a406941703bfaaa16c9f
Jonas Jensen <jdudal11@gmail.com>
2026-01-11 22:08:08 +0100

    arguments: Added arguments to program input
    To actually use this program, arguments needs to be passed to the
    solver, so we can get the corresponding Julia set. This commit checks the
    arguments, converts them to doubles, and calculates the R value.
    
    It was also discovered while working on different complex constants that
    the lightness value was not capped, and would therefore result in weird
    colors when the value was too high, this was fixed with a clamp.

--- src/pi-julia-set.c
+++ src/pi-julia-set.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
@@ -13,12 +15,36 @@
 #define IMAGESIZE (ROWSIZE * HEIGHT) // size of image data
 #define SIZE (HEADER + IMAGESIZE) // header data + image data
 
-#define R 1.251105362355361781933320// escape radius of Julia set
 #define MAX_ITERATION 1000
 
-double julia(double zx, double zy) {
-	double cx = M_PI / 10.0;
-	double cy = 0.0;
+double to_double(char * str) {
+	uint32_t i;
+	double div = 1.0;
+
+	uint32_t out_int = 0;
+	uint32_t out_dec = 0;
+
+	char *decimal_place = strchr(str, '.');
+
+	out_int = atoi(str);
+
+	if (decimal_place) {
+		*decimal_place = 0;
+		out_dec = atoi(decimal_place + 1);
+
+		for (i = 0; i < strlen(decimal_place + 1); i++) {
+			div /= 10.0;
+		}
+	}
+
+	return out_int + out_dec * div;
+}
+
+double calc_r(double cx, double cy) {
+	return 0.5 * (sqrt(4 * sqrt(cx * cx + cy * cy) + 1) + 1);
+}
+
+double julia(double zx, double zy, double cx, double cy, double R) {
 	double smooth_color = exp(-sqrt(zx * zx + zy * zy));
 
 	uint32_t iteration = 0;
@@ -35,7 +61,7 @@ double julia(double zx, double zy) {
 	return smooth_color;
 }
 
-int main() {
+int main(int argc, char ** argv) {
 	// Initialize bitmap
 	bmp_t bitmap;
 
@@ -46,12 +72,21 @@ int main() {
 		return 1;
 	}
 
+	if (argc < 2) {
+		printf("Error: Not enough arguments\n\nUSAGE: %s <cr> <ci>\n", argv[0]);
+		return 1;
+	}
+
+	double cx = to_double(argv[1]);
+	double cy = to_double(argv[2]);
+	double R = calc_r(cx, cy);
+
 	// Calculate Julia set value for each pixel, convert value to hsl, then rgb and write to bitmap
 	for (uint32_t y = 0; y < HEIGHT; y++) {
 		double zy = (double)y / HEIGHT * 2 * R - R;
 		for (uint32_t x = 0; x < WIDTH; x++) {
 			double zx = (double)x / HEIGHT * 2 * R - R;
-			double val = julia(zx, zy);
+			double val = julia(zx, zy, cx, cy, R);
 
 			double h,s,l;
 			char r,g,b;
